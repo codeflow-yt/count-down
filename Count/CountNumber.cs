6efa2ad16d138980a65681b88091bfe8067bb1d8
wekonu <wahjoe.eko.n@gmail.com>
2021-05-26 16:02:04 +0700

    [MAJOR ADD] initialize project files

---
+++ Count/CountNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Count
+{
+    public class CountNumber
+    {
+        enum CountDirection
+        {
+            FW,
+            BW
+        };
+
+        private int currentNumber = 0;
+        private int countStartOn = 0;
+        private int countEndOn = 0;
+
+        private CountDirection countDirection;
+
+        public CountNumber(int countStartOn = 0, int countEndOn = 10)
+        {
+            this.countStartOn = countStartOn;
+            this.countEndOn = countEndOn;
+            this.currentNumber = countStartOn;
+
+            this.countDirection = countEndOn > countStartOn ? CountDirection.FW : CountDirection.BW;
+        }
+
+        public void SetToLabel(Label label, Vector dimension)
+        {
+            label.Text = this.currentNumber.ToString();
+            CenterizedLabel(label, dimension);
+        }
+
+        public void Count()
+        {
+            if (this.countDirection == CountDirection.FW && this.currentNumber < this.countEndOn)
+            {
+                this.currentNumber++;
+            }
+            else if (this.countDirection == CountDirection.BW && this.currentNumber > this.countEndOn)
+            {
+                this.currentNumber--;
+            }
+        }
+
+        private void CenterizedLabel(Label label, Vector dimension)
+        {
+            int x = Convert.ToInt32((dimension.X / 2) - (label.Width / 2));
+            int y = Convert.ToInt32((dimension.Y / 2) - (label.Height / 2));
+
+            label.Location = new System.Drawing.Point(x, y);
+        }
+    }
+}
---
+++ Count/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Count
+{
+    public partial class Form1 : Form
+    {
+        CountNumber countNumber = new CountNumber(0, 99);
+
+        Vector formDimension = new Vector();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            formDimension.X = this.Width;
+            formDimension.Y = this.Height;
+
+            countNumber.SetToLabel(label1, formDimension);
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            countNumber.Count();
+
+            countNumber.SetToLabel(label1, formDimension);
+        }
+    }
+}
